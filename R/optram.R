c54df333dd868c95727ada1c76c65a09029dd5c0
Micha Silver <silverm@post.bgu.ac.il>
2023-03-28 19:06:06 +0300

    Final edits to optram.R

--- R/optram.R
+++ R/optram.R
@@ -39,10 +39,11 @@ optram <- function(aoi_file,
   # Avoid "no visible binding for global variable" NOTE
   access_ok <- s2_list <- s2_dirs <- BOA_dir <- NULL
   VI_dir <- VI_list <- VI_STR_df <- coeffs <- NULL
-  
+
   # Make sure we have access to scihub
   optram_func <- as.character(match.call()[[1]])
-  access_ok <- rOPTRAM::check_scihub_access(scihub_user, scihub_pass, optram_func)
+  access_ok <- rOPTRAM::check_scihub_access(scihub_user,
+                                            scihub_pass, optram_func)
   if (! access_ok) {
       return(NULL)
   }
