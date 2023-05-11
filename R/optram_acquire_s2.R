451012fb66aa4fc0be898b44d8460e65e7a032bd
Micha Silver <silverm@post.bgu.ac.il>
2023-05-11 18:38:57 +0300

    Adds dir.create to acquire_s2 for output_dir

--- R/optram_acquire_s2.R
+++ R/optram_acquire_s2.R
@@ -94,6 +94,10 @@ optram_acquire_s2 <- function(
   aoi_name  <- result_list <- NULL
   # Checks OK, proceed to download
   aoi_name <- rOPTRAM::aoi_to_name(aoi_file)
+  # Make sure output_dir exists
+  if (!dir.exists(output_dir)) {
+    dir.create(output_dir)
+  }
   # Get generic config json 
   config_file <- system.file("extdata", "s2_config.json", package = "rOPTRAM")
   result_list <- sen2r::sen2r(
