f7bd4d236a057103b66b16023b40f3f3685efc85
Natalya <npanov5856@gmail.com>
2023-07-11 16:51:12 +0300

    Now it works, all outputs are created

---
+++ R/call_optram_landsat.R
@@ -0,0 +1,17 @@
+# call optram_landsat
+library(devtools)
+load_all()
+
+landsat_dir = "C:/Users/Natalya/Downloads/landsat_oPTRAM"
+aoi_file <- "D:/rOPTRAM/aoi/migda_perimeter.shp"
+vi = "NDVI"
+LC_output_dir = "D:/rOPTRAM/derived_products"
+data_output_dir = "D:/rOPTRAM/output"
+
+#' @return coeffs, list, the derived trapezoid coefficients
+
+optram_landsat (landsat_dir,
+                aoi_file,
+                vi = 'NDVI',
+                LC_output_dir,
+                data_output_dir)
--- R/optram_landsat_try.R
+++ R/optram_landsat_try.R
@@ -20,7 +20,10 @@
 #landsat_dir = "C:/Users/Natalya/Downloads/landsat_oPTRAM"
 
 #' @param aoi_file, string, path to boundary polygon spatial file of area of interest
-#aoi_file <- "D:/rOPTRAM/aoi"
+#???????????????????????????????????????????????????
+# What is correct: only path, or path with the name?
+# 1. aoi_file <- "D:/rOPTRAM/aoi" # aoi_name = "aoi"
+# 2. aoi_file <- "D:/rOPTRAM/aoi/migda_perimeter.shp" # aoi_name = "migdaperimeter"
 
 #' @param vi, string, which VI to prepare, either 'NVDI' (default) or 'SAVI' or 'MSAVI'
 #vi = "NDVI"
@@ -34,12 +37,18 @@
 #data_output_dir = "D:/rOPTRAM/output"
 
 #' @return coeffs, list, the derived trapezoid coefficients
+# In the following FUN call all data is saved in tmp folders
+# optram_landsat <- function(landsat_dir,
+#                       aoi_file,
+#                        vi = 'NDVI',
+#                        LC_output_dir = tempdir(),
+#                        data_output_dir = tempdir()) {
 
-optram_landsat <- function(landsat_dir,
-                        aoi_file,
-                        vi = 'NDVI',
-                        LC_output_dir = tempdir(),
-                        data_output_dir = tempdir()) {
+  optram_landsat <- function(landsat_dir,
+                             aoi_file,
+                             vi = 'NDVI',
+                             LC_output_dir,
+                             data_output_dir) {
 
     # Avoid "no visible binding for global variable" NOTE
     landsat_list <- band_ids <- aoi <- derived_rasters <- xml_file <- NULL
@@ -51,20 +60,18 @@ optram_landsat <- function(landsat_dir,
     # create NDVI and STR indices for each and crop to aoi
 
 # TODO: How to recognize folder of Landsat imagery? Landsat_list - folders with images
-    landsat_list <- list.dirs(landsat_dir, full.names = TRUE, recursive = TRUE)
-    landsat_list <- landsat_list[grepl(pattern = "L*_02_T1", x = landsat_list)]
 # MS:
 # The list.dirs() function takes a 'pattern=' argument. So above two lines can be:
-    landsat_list <- list.dirs(landsat_dir,
-                            pattern = "L.*_02_T.*TIF$",
-                            full.names = TRUE, recursive = TRUE)
+#    landsat_list <- list.dirs(landsat_dir,
+#                            pattern = "L.*_02_T.*TIF$",
+#                           full.names = TRUE, recursive = TRUE)
+# N:
+# this is working:
+    landsat_list <- list.dirs(landsat_dir)[grepl("L*_02_T1",list.dirs(landsat_dir))]
 
 # https://www.usgs.gov/faqs/how-do-i-use-a-scale-factor-landsat-level-2-science-products
-#  meanwhile scaling is set: gain and offset. There is extraction from metadata down
-    gain <- 0.0000275
-    offset <- -0.2
+# There is extraction from metadata down
 
-    #TODO: Which bands?
     # The strings below are used to select the needed bands from Landsat
     # L89 - bands for LANDSAT 8/9
     # L57 - bands for LANDSAT 5/7
@@ -78,22 +85,24 @@ optram_landsat <- function(landsat_dir,
       "_SR_B4", #NIR wide
       "_SR_B7"  #SWIR 2200
     )
-    # Get Area of interest
-    #?????????????????????????? Warning message:
-    #                           [vect] Z coordinates ignored
-    aoi <- terra::vect(aoi_file)
 
     if (length(landsat_list) == 0) {
         message("No landsat folders in: ", landsat_dir, " directory", "\n",
         "Please check download folder.", "\n", "Exiting...")
         return(NULL)
     }
-    # ???????????????????????????????? aoi_to_name
-    #                                  Error in aoi_to_name(aoi_file) :
-    # could not find function "aoi_to_name"
+
+    # crs: from the first tile in the first landsat dir
+    rstt <- terra::rast(dir(landsat_list[1], full.names = TRUE)[grepl("*_SR_B1.TIF$", dir(landsat_list[1]))])
+    epsg_code <- paste("EPSG:",
+                       (as.character(terra::crs(rstt, describe=T)[3])))
+    # Get Area of interest
+    #?????????????????????????? Warning message:
+    #                           [vect] Z coordinates ignored
+    aoi <- terra::vect(aoi_file)
+    aoi <- terra::project(aoi, epsg_code)
     aoi_name <- aoi_to_name(aoi_file)
-    # meanwhile
-    aoi_name <- "MIGDA"
+
     # Prepare output directories
     BOA_dir <- file.path(LC_output_dir, "BOA")
     if (!dir.exists(BOA_dir)) {
@@ -114,13 +123,8 @@ optram_landsat <- function(landsat_dir,
     # derived_rasters FUN uses landsat images folders. Output is derived_rasters
     #s <- landsat_list # only to check this FUN
     # s=s[1] # for test only
-    derived_rasters <- lapply(landsat_list, function(s) {
-#        xml_file <- list.files(s, pattern = "MTL.*xml$", full.names = TRUE)[1]
-#        xml <- xml2::read_xml(xml_file)
-#        img_nodes <- xml2::xml_find_all(xml, ".//IMAGE_FILE")
-#        img_nodes <- img_nodes[!grepl(pattern = "R60m", img_nodes)]
-#        img_paths <- xml2::xml_contents(img_nodes)
 
+    derived_rasters <- lapply(landsat_list, function(s) {
         # Get CRS for this landsat dataset, and reproject AOI
         mtl_file <- list.files(s, pattern = "MTL.*xml$",
                                 recursive = TRUE, full.names = TRUE, )[1]
@@ -129,16 +133,12 @@ optram_landsat <- function(landsat_dir,
             return(NULL)
         }
         mtl <- xml2::read_xml(mtl_file)
-
-# TODO: How to get the coordinate system from metadata file
         # epsg will be taken from the first image.
         # take gain and offset.
         gain <- xml2::xml_text(xml2::xml_find_first(mtl, ".//REFLECTANCE_MULT_BAND_1"))
         offset <- xml2::xml_text(xml2::xml_find_first(mtl, ".//REFLECTANCE_ADD_BAND_1"))
         gain <- as.numeric(gain)
         offset <- as.numeric(offset)
-#        aoi <- terra::project(aoi, epsg_code)
-
         # Read in tifs
         if (grepl("LC08", s) | grepl("LC09", s)) {
           band_ids <- band_L89
@@ -148,45 +148,18 @@ optram_landsat <- function(landsat_dir,
         }
        img_list <- lapply(band_ids, function(b){
           # img_path - full names in only one landsat folder
-          # img_path <- img_paths[grepl(pattern = b, img_path, fixed = TRUE)]
           # filter only sr bands
           img_path <- dir(s)[grepl(pattern = "*_SR_B[0-9]*.TIF$", x = dir(s))]
-          # read the first raster in the list to take crs
-          rstt <- terra::rast(file.path(s, img_path)[1])
-          epsg_code <- paste("EPSG:",
-                            (as.character(terra::crs(rstt, describe=T)[3])))
-          aoi <- terra::vect(aoi_file)
-          aoi <- terra::project(aoi, epsg_code)
+
           img_path <- img_path[grepl(pattern = b, img_path, fixed = TRUE)]
 
 #MS: Are you going to use static values for gain and offset?
 # or read from the XML metadata?
+#N: values for gain and offset are read from the XML metadata
           gain <- xml2::xml_text(xml2::xml_find_first(mtl, ".//REFLECTANCE_MULT_BAND_1"))
           offset <- xml2::xml_text(xml2::xml_find_first(mtl, ".//REFLECTANCE_ADD_BAND_1"))
           gain <- as.numeric(gain)
           offset <- as.numeric(offset)
-#         aoi <- terra::project(aoi, epsg_code)
-
-        # Read in tifs
-          if (grepl("LC08", s) | grepl("LC09", s)) {
-            band_ids <- band_L89
-          }
-          if (grepl("LE07", s) | grepl("LT05", s)) {
-            band_ids <- band_L57
-          }
-          img_list <- lapply(band_ids, function(b){
-            # img_path - full names in only one landsat folder
-            # img_path <- img_paths[grepl(pattern = b, img_path, fixed = TRUE)]
-            # filter only sr bands
-            img_path <- dir(s)[grepl(pattern = "*_SR_B[0-9]*.TIF$", x = dir(s))]
-            # read the first raster in the list to take crs
-            rstt <- terra::rast(file.path(s, img_path)[1])
-
-          epsg_code <- paste("EPSG:",
-                          (as.character(terra::crs(rstt, describe=T)[3])))
-          aoi <- terra::vect(aoi_file)
-          aoi <- terra::project(aoi, epsg_code)
-          img_path <- img_path[grepl(pattern = b, img_path, fixed = TRUE)]
 
 # TODO: what file extension of original Landsat imagery
 #         img_file <- paste0(img_path, ".jp2")
@@ -194,20 +167,6 @@ optram_landsat <- function(landsat_dir,
           rst <- terra::rast(img_path, win = terra::ext(aoi))
           return(rst)
         })  # end-of-img_list is working. the result is img_list
-            # Make a rast obj to save the high resolution extent
-            # The first raster in the list is blue, 10m. Use for reampling
-# TODO: What resolution? 30m... For Landsat I don't need this function.
-#        img_10m <- img_list[[1]]
-#        img_10m_list <- lapply(img_list, function(i) {
-#            if (all(terra::res(i) == c(10, 10))) {
-#                return(i)
-#            } else {
-#                return(terra::resample(i, img_10m,
-#                                      method = "bilinear",
-#                                      threads = TRUE))
-#            }
-#        }) # enf-of img_10_list
-#        img_stk <- terra::rast(img_10m_list)
 
         img_stk <- terra::rast(img_list)
         img_stk <- img_stk*gain + offset
@@ -215,31 +174,24 @@ optram_landsat <- function(landsat_dir,
         # Save to BOA dir
         # Create filename
         # Prepare file name parts for saving rasters
-        # the name is formed fron folder
+        # the name is formed from folder
         s_parts <- unlist(strsplit(basename(s), "_"))
         BOA_file <- paste(s_parts[1], s_parts[2],s_parts[3],s_parts[4],s_parts[6],
                         aoi_name, "BOA.tif", sep = "_")
         terra::writeRaster(img_stk,
                          file.path(BOA_dir, BOA_file), overwrite = TRUE)
         return(img_stk)
-    }) # till this point it is working, except FUN "aoi_to_name"
-
+    }) # enf-of-derived_rasters
 
     # Get VI and STR from this list of raster stacks
     VI_STR_list <- lapply(1:length(derived_rasters), function(x) {
-      # Each item in the derived_rasters list is a raster stack, with 6 bands
-      # R-G-B-NIR, SWIR 1600, SWIR 2200
-#      stk <- derived_rasters[[x]] this isn't working, works [x]
 # LANDSAT: Each item in the derived_rasters list is a raster stack, with 3 bands:
-# R-NIR, SWIR 2200
-      stk <- derived_rasters[x]
+# R, NIR, SWIR 2200
+      stk <- derived_rasters[[x]]
       if (is.null(stk)) {
         return(NULL)
       }
-
       # Use the metadata file from landsat directory name to get image date
-
-      # TODO: How to extract image date from metadata?
       s <- landsat_list[x]
       # Prepare file name parts for saving rasters
 #      s_parts <- unlist(strsplit(basename(s), "_"))
@@ -262,17 +214,13 @@ optram_landsat <- function(landsat_dir,
       }
 #extract time, date and create datetime
         mtl <- xml2::read_xml(mtl_file)
-        timestr <- xml2::xml_text(xml2::xml_find_first(mtl, ".//SCENE_CENTER_TIME"))
-        datestr <- xml2::xml_text(xml2::xml_find_first(mtl, ".//DATE_ACQUIRED"))
+#        timestr <- xml2::xml_text(xml2::xml_find_first(mtl, ".//SCENE_CENTER_TIME"))
+        datestr <- as.Date(xml2::xml_text(xml2::xml_find_first(mtl, ".//DATE_ACQUIRED")))
+#        datestr <- xml2::xml_text(xml2::xml_find_first(mtl, ".//DATE_ACQUIRED"))
+#        datestr1 <- paste0(datestr, "T",timestr) # datestr1 =  "2022-06-11T08:11:14.1035130Z"
+#        datetime <- strptime(datestr1, format = "%FT%X", tz = "UTC")
+#        datetime = "2022-06-11 08:11:14 UTC"
 
-        datestr1 <- paste0(datestr, "T",timestr) # datestr1 =  "2022-06-11T08:11:14.1035130Z"
-        datetime <- strptime(datestr1, format = "%FT%X", tz = "UTC")
-        # datetime = "2022-06-11 08:11:14 UTC"
-
-#      VI_idx <- rOPTRAM::calculate_vi(stk, vi, redband = 3, nirband = 4)
-#      VI_df <- terra::as.data.frame(VI_idx, xy = TRUE)
-      # Add image date to dataframe
-#      VI_df['Date'] <- datestr
 
       VI_idx <- rOPTRAM::calculate_vi(stk, vi, redband = 1, nirband = 2)
       VI_df <- terra::as.data.frame(VI_idx, xy = TRUE)
@@ -288,20 +236,20 @@ optram_landsat <- function(landsat_dir,
       # Save VI to NDVI_dir
       # Prepare file name parts for saving rasters
       s_parts <- unlist(strsplit(basename(landsat_list[x]), "_"))
-      VI_file <- paste(s_parts[1], s_parts[3], s_parts[5],
+      VI_file <- paste(s_parts[1], s_parts[2],s_parts[3],s_parts[4],s_parts[6],
                        aoi_name, "NDVI.tif", sep = "_")
       terra::writeRaster(VI_idx,
                          file.path(NDVI_dir, VI_file), overwrite = TRUE)
       # Save STR to BOA_dir
-      STR_file <- paste(s_parts[1], s_parts[3], s_parts[5],
+      STR_file <- paste(s_parts[1], s_parts[2],s_parts[3],s_parts[4],s_parts[6],
                         aoi_name, "STR.tif", sep = "_")
       terra::writeRaster(STR,
                          file.path(STR_dir, STR_file), overwrite = TRUE)
 
-      return(full_df)
+      return(full_df) # end-of-VI_STR_list
     })
 
-    full_VI_STR <- do.call(rbind, VI_STR_list)
+        full_VI_STR <- do.call(rbind, VI_STR_list)
     # SAve full data.frame to work_dir
     full_df_path <- file.path(data_output_dir, "VI_STR_data.rds")
     saveRDS(full_VI_STR, full_df_path)
@@ -310,17 +258,6 @@ optram_landsat <- function(landsat_dir,
     coeffs <- rOPTRAM::optram_wetdry_coefficients(full_VI_STR,
                                                   aoi_file,
                                                   data_output_dir)
+
     return(coeffs)
-    })
-}
-
-
-
-
-
-
-
-
-
-
-
+    }
