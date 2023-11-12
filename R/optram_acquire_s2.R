6546714e19639f8eb3a8ee99ef2d9d7efce93dd9
Micha Silver <silverm@post.bgu.ac.il>
2023-11-12 18:31:30 +0200

    Fixes typo in acquire_s2

--- R/optram_acquire_s2.R
+++ R/optram_acquire_s2.R
@@ -94,7 +94,7 @@ optram_acquire_s2 <- function(
   }
 
   gcloud_ok <- sen2r::check_gcloud(gsutil_path, check_creds = FALSE)
-  if (gcloud_id) {
+  if (gcloud_ok) {
     message("Using gcloud")
     servers <- ("gcloud")
   } else {
