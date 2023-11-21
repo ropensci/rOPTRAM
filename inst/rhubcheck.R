c06ca3d893748e3e48d125d497b0b81854c58d93
Micha Silver <silverm@post.bgu.ac.il>
2023-11-21 12:15:17 +0200

    Fixes output of running time

--- inst/rhubcheck.R
+++ inst/rhubcheck.R
@@ -49,7 +49,9 @@ print(res, row.names = FALSE)
 t1 <- Sys.time()
 print(paste(t1, " - Check completed"))
 interval <- as.numeric(difftime(t1, t0), units = "mins")
-print(paste("Running time:"), interval, "minutes")
+print(paste("Checking duration: "), 
+            sprintf("%.1f", interval),
+            "minutes")
 
 if (any(colSums(res[2L:3L]) > 0)) {
   stop("Some checks with ERROR, or WARNING.")
