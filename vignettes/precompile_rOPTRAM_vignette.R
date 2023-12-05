3dd1d3e5c1606a7fc141cbb4cac138161dfc16be
Micha Silver <silverm@post.bgu.ac.il>
2023-12-05 13:18:25 +0200

    Uses here() to make vignette portable

--- vignettes/precompile_rOPTRAM_vignette.R
+++ vignettes/precompile_rOPTRAM_vignette.R
@@ -2,7 +2,7 @@ knitr::knit(input = "vignettes/rOPTRAM.Rmd.orig",
             output = "vignettes/rOPTRAM.Rmd")
 
 # Output directory and plot file name as defined in the vignette:
-output_dir <- file.path(tempdir(), "full")
+output_dir <- here::here("inst", "extdata", "S2A", "full")
 figures <- c("trapezoid_Migda.png")
 invisible(lapply(figures, function(f) {
   fig_path <- file.path(output_dir, f)
