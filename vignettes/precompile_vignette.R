5899ceff7a2d4bc28bf0a933d1d38533e94f3597
Micha Silver <silverm@post.bgu.ac.il>
2023-12-13 14:57:07 +0200

    Modifies output_dir for vignette precompile

--- vignettes/precompile_vignette.R
+++ vignettes/precompile_vignette.R
@@ -2,12 +2,11 @@ knitr::knit(input = "vignettes/rOPTRAM.Rmd.orig",
             output = "vignettes/rOPTRAM.Rmd")
 
 # Output directory and plot file name as defined in the vignette:
-output_dir <- here::here("inst", "extdata", "S2A", "full")
+output_dir <- file.path(tempdir(), "full")
 figures <- c("trapezoid_Migda.png")
 invisible(lapply(figures, function(f) {
   fig_path <- file.path(output_dir, f)
-  file.copy(from = fig_path,
-              to = f)
+  file.copy(from = fig_path, to = f)
   }))
 
 devtools::build_vignettes()
