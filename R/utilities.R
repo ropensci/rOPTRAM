8cee2567de23ba8e5b32192c53843daa3e112e33
Micha Silver <silverm@post.bgu.ac.il>
2023-12-14 18:38:27 +0200

    Work around to Include leaflet map into vignette

--- R/utilities.R
+++ R/utilities.R
@@ -81,7 +81,7 @@ check_aoi <- function(aoi_file) {
 
 check_date_string <- function(date_string) {
   if (!is.character(date_string)) return(FALSE)
-  yr <- unlist(strsplit(date_string), "-")[1]
+  yr <- unlist(strsplit(date_string, "-"))[1]
   if (nchar(yr) != 4) return(FALSE)
   d <- as.Date(date_string, format = "%Y-%m-%d")
   if (is.na(d)) return(FALSE)
--- vignettes/precompile_vignette.R
+++ vignettes/precompile_vignette.R
@@ -3,10 +3,11 @@ knitr::knit(input = "vignettes/rOPTRAM.Rmd.orig",
 
 # Output directory and plot file name as defined in the vignette:
 output_dir <- file.path(tempdir(), "full")
-figures <- c("trapezoid_Migda.png")
+figures <- c("trapezoid_Migda.png", "sm_map.rds")
 invisible(lapply(figures, function(f) {
   fig_path <- file.path(output_dir, f)
-  file.copy(from = fig_path, to = f)
+  vig_path <- file.path("vignettes", f)
+  file.copy(from = fig_path, to = vig_path)
   }))
 
 devtools::build_vignettes()
