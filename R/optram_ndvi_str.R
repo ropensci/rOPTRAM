afe32fec56603efb10b13bc7ac21fdf18ef7f174
Micha Silver <silverm@post.bgu.ac.il>
2023-12-31 17:36:22 +0200

    Changes to inner_join in optram_ndvi_str.R()

--- R/optram_ndvi_str.R
+++ R/optram_ndvi_str.R
@@ -107,7 +107,8 @@ optram_ndvi_str <- function(STR_list, VI_list,
   VI_df <- do.call(rbind, VI_df_list)
 
   # Merge VI and STR pixel data
-  full_df <- dplyr::left_join(VI_df, STR_df, by = c("x", "y"))
+  full_df <- dplyr::inner_join(VI_df, STR_df,
+                               by = c("x", "y", "Date"))
   full_df <- full_df[stats::complete.cases(full_df),]
   df_file <- file.path(output_dir, "VI_STR_data.rds")
   saveRDS(full_df, df_file)
