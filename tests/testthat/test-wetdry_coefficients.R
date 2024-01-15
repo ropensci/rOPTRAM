89a5c794020079d01c2d713bd0315f48cf49f88b
Micha Silver <silverm@post.bgu.ac.il>
2024-01-15 16:50:26 +0200

    Change to migda instead of migda_aoi

--- tests/testthat/test-wetdry_coefficients.R
+++ tests/testthat/test-wetdry_coefficients.R
@@ -22,7 +22,7 @@ test_that("aoi_file does not exist", {
 
 test_that("Output coefficients is a data.frame of length 4", {
   full_df <- readRDS(system.file("extdata", "VI_STR_data.rds", package = "rOPTRAM"))
-  aoi_file <- system.file("extdata", "migda_aoi.gpkg", package = "rOPTRAM")
+  aoi_file <- system.file("extdata", "migda.gpkg", package = "rOPTRAM")
   coeffs <- optram_wetdry_coefficients(full_df, aoi_file)
   expect_true(inherits(coeffs, "data.frame"))
   expect_equal(length(coeffs), 4)
