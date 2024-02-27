fac8864c240b5f589fdb195da6373a0f40f194b5
Micha Silver <silverm@post.bgu.ac.il>
2024-02-27 13:19:55 +0200

    Completes pre_compile script for vignettes

--- vignettes/precompile_vignette.R
+++ vignettes/precompile_vignette.R
@@ -1,12 +1,17 @@
 knitr::knit(input = "vignettes/rOPTRAM.Rmd.orig",
             output = "vignettes/rOPTRAM.Rmd")
 
+knitr::knit(input = "vignettes/rOPTRAM_trapezoid_methods.Rmd.orig",
+            output = "vignettes/rOPTRAM_trapezoid_methods.Rmd")
+
 # Output directory and plot file name as defined in the vignette:
-output_dir <- file.path(tempdir(), "full")
-figures <- c("trapezoid_Migda.png", "sm_map.rds")
+output_dir <- file.path(tempdir())
+figures <- c("trapezoid_Migda_9_linear.png",
+             "trapezoid_Migda_9_exponential.png",
+             "trapezoid_Migda_9_polynomial.png")
 invisible(lapply(figures, function(f) {
   fig_path <- file.path(output_dir, f)
-  vig_path <- file.path("vignettes", f)
+  vig_path <- file.path("vignettes", "images", f)
   file.copy(from = fig_path, to = vig_path)
   }))
 
