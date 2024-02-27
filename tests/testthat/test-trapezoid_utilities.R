c1cc1942a2a8b8a919ddc2635b9480ba64d2e06d
micha-silver <tsvibar@gmail.com>
2024-02-27 16:52:14 +0200

    Adds test for number of coefficients in soil moisture functions

--- tests/testthat/test-trapezoid_utilities.R
+++ tests/testthat/test-trapezoid_utilities.R
@@ -13,6 +13,32 @@ test_that("Trapezoid methods return data.frame", {
   expect_equal(ncol(res), 5)
 })
 
+test_that("Check for correct number of coefficients", {
+  img_date <- "2023-01-25"
+  VI_file <- system.file("extdata", "SAVI",
+                         paste0("SAVI_", img_date, ".tif"),
+                         package = "rOPTRAM")
+  VI <- terra::rast(VI_file)
+  STR_file <- system.file("extdata", "STR",
+                          paste0("STR_", img_date, ".tif"),
+                          package = "rOPTRAM")
+  STR <- terra::rast(STR_file)
+  coeffs_csv <- system.file("extdata", "coefficients_lin.csv",
+                            package = "rOPTRAM")
+  coeffs <- read.csv(coeffs_csv)[1:3]
+  expect_null(linear_soil_moisture(coeffs, VI, STR))
+
+  coeffs_csv <- system.file("extdata", "coefficients_exp.csv",
+                            package = "rOPTRAM")
+  coeffs <- read.csv(coeffs_csv)[1:3]
+  expect_null(exponential_soil_moisture(coeffs, VI, STR))
+
+  coeffs_csv <- system.file("extdata", "coefficients_poly.csv",
+                            package = "rOPTRAM")
+  coeffs <- read.csv(coeffs_csv)[1:5]
+  expect_null(polynomial_soil_moisture(coeffs, VI, STR))
+})
+
 test_that("Soil moisture raster is returned", {
   img_date <- "2023-01-25"
   VI_file <- system.file("extdata", "SAVI",
@@ -39,4 +65,5 @@ test_that("Soil moisture raster is returned", {
   W <- polynomial_soil_moisture(coeffs, VI, STR)
   expect_true(inherits(W, "SpatRaster"))
 })
+
 
