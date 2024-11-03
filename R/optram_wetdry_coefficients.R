8066c1ce1779948186ec8e0320007cfe352a9912
Micha Silver <silverm@post.bgu.ac.il>
2024-11-03 11:25:50 +0200

    Density plot-colors to grey100

--- R/optram_wetdry_coefficients.R
+++ R/optram_wetdry_coefficients.R
@@ -233,7 +233,8 @@ plot_vi_str_cloud <- function(
                  alpha = 0.1, size = 0.3)
   } else if (plot_colors == "density") {
     pl <- ggplot2::ggplot(plot_df) +
-      geom_point(aes(x = VI, y = STR, color = "white", size=0.01)) +
+      geom_point(aes(x = VI, y = STR),
+                 color = "grey100", fill = "grey100", size=0.01) +
       geom_bin2d(aes(x = VI, y = STR), bins=c(120,120)) +
       scale_fill_viridis_c(direction = -1) +
       theme(legend.position = "none")
