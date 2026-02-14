1d9960ea16d9ad8bb4ac6ae477bcaa7e778aebd7
Micha Silver <silverm@post.bgu.ac.il>
2026-02-14 14:20:44 +0200

    Version 0.3.2, fix to STR evalscripts

--- inst/scripts/STR11.js
+++ inst/scripts/STR11.js
@@ -3,11 +3,12 @@
 function setup() {
     return {
         input: [{ // this sets which bands to use
-            bands: ["B11"]
+            bands: ["B11"],
+            units: "DN"
             }],
         output: { // this defines the output image type
             bands: 1,
-            sampleType: "INT16",
+            sampleType: "FLOAT32",
         }
     };
 }
@@ -15,10 +16,10 @@ function setup() {
 function evaluatePixel(sample) {
     var value = sample.B11;
     if (value != 0) {
-      v = value / 10000
+      var v = value / 10000.0;
       str = ((1 - v)**2) / (2*v);
     } else {
-      str = 0
+      str = 0;
     };
     return [ str ];
 }
